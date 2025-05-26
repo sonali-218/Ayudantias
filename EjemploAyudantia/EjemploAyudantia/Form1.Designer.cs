99b2a4bef16df3fc4ea8e75fdd2fcf976fbe3c05
sonali-218 <alisson.quijano@gmail.com>
2025-05-26 16:12:31 -0600

    Add files via upload

---
+++ EjemploAyudantia/EjemploAyudantia/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace EjemploAyudantia
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.Feedback = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(59, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(339, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Selecciona tu estructura de datos:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Tree",
+            "Graph",
+            "LinkedList"});
+            this.comboBox1.Location = new System.Drawing.Point(64, 122);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(189, 33);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Feedback
+            // 
+            this.Feedback.AutoSize = true;
+            this.Feedback.Location = new System.Drawing.Point(64, 199);
+            this.Feedback.Name = "Feedback";
+            this.Feedback.Size = new System.Drawing.Size(0, 25);
+            this.Feedback.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Feedback);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label Feedback;
+    }
+}
+
---
+++ EjemploAyudantia/EjemploAyudantia/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EjemploAyudantia
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ComboBox combo = sender as ComboBox;
+            if (combo.SelectedText != null)
+            {
+                Feedback.Text = "Has seleccionado: " + combo.SelectedItem.ToString();
+                Feedback.Visible = true;
+            }
+            else
+            {
+                Feedback.Text = "No has seleccionado nada";
+                Feedback.Visible = true;
+            }
+        }
+    }
+}
