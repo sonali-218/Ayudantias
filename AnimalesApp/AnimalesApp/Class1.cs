48a4e3774d2b259151bc9877d8f2bd5a2641c324
sonali-218 <alisson.quijano@gmail.com>
2025-05-26 15:37:47 -0600

    Add files via upload

---
+++ AnimalesApp/AnimalesApp/Class1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+interface Animal
+{
+    string Speak();
+    string Eat();
+    string Sleep();
+}
+
+class Pato : Animal
+{
+    public Pato()
+    { }
+
+    public string Speak()
+    {
+        return "Quack!";
+    }
+
+    public string Eat()
+    {
+        return "Pato esta comiendo";
+    }
+
+    public string Sleep()
+    {
+        return "Pato esta durmiendo";
+    }
+}
+
+// abstraccion - cuando no se conoce que va a comer el animal ej.
+public class Vaca : Animal
+{
+    public Vaca()
+    { }
+    public string Speak()
+    {
+        return "Muuu!";
+    }
+    public string Eat()
+    {
+        return "Vaca esta comiendo pasto";
+    }
+    public string Sleep()
+    {
+        return "Vaca esta durmiendo";
+    }
+}
+
+public class Class1
+{
+    public Class1()
+    { 
+
+    }
+}
---
+++ AnimalesApp/AnimalesApp/Program.cs
@@ -0,0 +1,13 @@
+﻿
+string animalType = Console.ReadLine();
+
+Animal animal = animalType switch
+{
+    "Pato" => new Pato(),
+    "Vaca" => new Vaca(),
+    _ => throw new ArgumentException("Tipo de animal no reconocido")
+};
+
+Console.WriteLine(animal.Speak());
+Console.WriteLine(animal.Eat());
+Console.WriteLine(animal.Sleep());
